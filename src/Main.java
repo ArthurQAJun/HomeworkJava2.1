e5095203e0ee3c762381f4bb791e98d85071e993
Arthur <rutra.ru@yandex.ru>
2023-02-07 19:37:33 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int milesperruble = 20;
+
+        int bonusmiles = price / milesperruble;
+        System.out.println(bonusmiles + " миль(ей)");
+
+    }
+}
